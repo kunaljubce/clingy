66dc8614665fe9ffca7eca03ceba5cf7cc98a6d7
Kunal Bhattacharya <kunal.jubce@gmail.com>
2020-11-15 22:59:46 +0530

    Adding Pip files and initial version of code - Code prints the count of messages in each label!

---
+++ get_gmail_data.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is created automatically when the authorization flow completes 
+    # for the first time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail API
+    results = service.users().labels().list(userId='me').execute()
+    #print(results)
+    labels = results.get('labels', [])
+
+    delete_msgs_from_labels = ['SPAM', 'CATEGORY_PROMOTIONS', 'CATEGORY_SOCIAL']
+
+    if not labels:
+        print('No labels found.')
+    else:
+        #print('Labels:')
+        for label in labels:
+            if label['name'] in delete_msgs_from_labels:
+                label_dtls = service.users().labels().get(userId = "me", id = label['name']).execute()
+                total_msg_count_in_label, unread_msg_count_in_label = label_dtls['messagesTotal'], label_dtls['messagesUnread']
+                print("Total Messages in", label['name'], ":", total_msg_count_in_label)
+                print("Unread Messages in", label['name'], ":", unread_msg_count_in_label)
+
+if __name__ == '__main__':
+    main()
