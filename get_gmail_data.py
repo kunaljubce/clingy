c90e17e6d1f1ded3bdc41e007418352f6f916dca
Kunal Bhattacharya <kunal.jubce@gmail.com>
2021-07-31 16:11:12 +0530

    Added spaces

--- get_gmail_data.py
+++ get_gmail_data.py
@@ -52,6 +52,7 @@ def main():
     Lists the user's Gmail labels.
     """
     creds = None
+    
     # The file token.json stores the user's access and refresh tokens, and is created automatically when 
     # the authorization flow completes for the first time.
     if os.path.exists('token.json'):
@@ -64,6 +65,7 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
+
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
