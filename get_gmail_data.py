f1505c274ee7a56e60b8704f16ccc1cf62a70793
Kunal Bhattacharya <kunal.jubce@gmail.com>
2021-07-31 15:07:58 +0530

    Modified authentication mechanism as per latest gmail api standards

--- get_gmail_data.py
+++ get_gmail_data.py
@@ -1,3 +1,7 @@
+#############################################################################################################################################
+# Command to run: pipenv run python3 get_gmail_data.py --labels-and-types '{"category_promotions":"read"}'                                  #
+#############################################################################################################################################
+
 from __future__ import print_function
 import json
 import pickle
@@ -6,6 +10,7 @@ import argparse
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
 parser = argparse.ArgumentParser(description='Pass some parameters.')
 parser.add_argument('--labels-and-types', default='{"spam":"all", "category_social":"all", "category_promotions":"all"}', type=json.loads, \
@@ -50,9 +55,8 @@ def main():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is created automatically when the 
     # authorization flow completes for the first time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -62,8 +66,8 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
     service = build('gmail', 'v1', credentials=creds)
 
