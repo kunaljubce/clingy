b2bd4cbd620f2cbaddd9c4ee5b7098e22e8d9910
Kunal Bhattacharya <kunal.jubce@gmail.com>
2023-10-08 13:03:58 +0530

    Code changes to write msg metadata to db

--- src/archive_msg_metadata.py
+++ src/archive_msg_metadata.py
@@ -1,10 +1,10 @@
 import sqlite3 as lite
-from typing import List
+from typing import List, Dict, Any
 from urllib.request import pathname2url
 
 CREATE_TABLE_QUERY = '''
     CREATE TABLE IF NOT EXISTS message_details(
-        msg_id TEXT PRIMARY KEY, --AUTOINCREMENT
+        msg_id TEXT PRIMARY KEY,
         subject TEXT,
         sender_email TEXT,
         recipient_email TEXT,
@@ -30,7 +30,7 @@ INSERT_MSG_METADATA_QUERY = '''
             '{return_path}')
 '''
 
-def db_conn(label_name, msg_metadata):
+def db_conn(label_name: str) -> object:
     """
     Setup SQLite DB connection and create message_details table if not exists.
     Inputs: label_name -> This attribute is used to create seperate DBs for each label.
@@ -49,17 +49,15 @@ def db_conn(label_name, msg_metadata):
         sqliteConnection = lite.connect(db_uri, uri=True)
         print("Creating DB!")"""
 
-    try:
-        # Connect to DB and create a cursor
-        sqliteConnection = lite.connect(db_name)
-        print('Instance of SQLite DB initiated!')
+    # Connect to DB and create a cursor
+    sqliteConnection = lite.connect(db_name)
+    print('Instance of SQLite DB initiated!')
+            
+    return sqliteConnection
 
-        query = 'Creating msg_details table...'
-        cursor = sqliteConnection.cursor()
-        sqliteConnection.execute(CREATE_TABLE_QUERY)
-        result = cursor.fetchall()
-        
-        insert_query = INSERT_MSG_METADATA_QUERY.format(
+def db_insert(label_name: str, msg_metadata: Dict[str, Any]) -> None:
+
+    insert_query = INSERT_MSG_METADATA_QUERY.format(
                                 msg_id = msg_metadata.get('msg_id'),
                                 subject = msg_metadata.get('subject', '').replace("'", '"'),
                                 sender_email = msg_metadata.get('from').replace("'", '"'),
@@ -70,23 +68,10 @@ def db_conn(label_name, msg_metadata):
                                 list_unsubscribe = msg_metadata.get('list_unsubscribe', '').replace("'", '"'),
                                 return_path = msg_metadata.get('return_path', '').replace("'", '"')
                             )
-        
+
+    query = 'Creating msg_details table...'
+
+    with db_conn(label_name) as sqliteConnObj:
+        sqliteConnObj.execute(CREATE_TABLE_QUERY)
+        cursor = sqliteConnObj.cursor()
         cursor.execute(insert_query)
-        #print(insert_query)
-        print("Message ID - {msg_id} - inserted into table!".format(msg_id=msg_metadata.get('msg_id')))
-        cursor.close()
-
-    # Handle errors
-    except lite.Error as error:
-        print('Error occurred - ', error)
-
-    # Close DB Connection irrespective of success or failure
-    finally:
-        if sqliteConnection:
-            sqliteConnection.close()
-            print('SQLite Connection closed')
-
-""" def db_insert(sqliteConnObj: object):
-    with sqliteConnObj.cursor() as cursor:
-
- """
