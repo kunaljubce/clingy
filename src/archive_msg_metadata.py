01876a341be6435001772ee2ce280446add951ee
Kunal Bhattacharya <kunal.jubce@gmail.com>
2024-01-01 20:39:51 +0530

    Added update table logic to provide two archival options to user - archive all msgs including those already deleted in past executions and only archive current batch of msgs to be deleted

--- src/archive_msg_metadata.py
+++ src/archive_msg_metadata.py
@@ -1,6 +1,10 @@
+import os
 import sqlite3 as lite
 from typing import List, Dict, Any
 from urllib.request import pathname2url
+from utils.config import get_clingy_app_download_path
+
+db_folder_path = get_clingy_app_download_path()
 
 CREATE_TABLE_QUERY = '''
     CREATE TABLE IF NOT EXISTS message_details(
@@ -9,13 +13,14 @@ CREATE_TABLE_QUERY = '''
         sender_email TEXT,
         recipient_email TEXT,
         msg_snippet BLOB,
-        date TEXT,
+        msg_receipt_date TEXT,
         reply_to TEXT,
         list_unsubscribe TEXT,
-        return_path TEXT
+        return_path TEXT,
+        record_insert_date DATETIME,
+        archival_cycle TEXT
     );
 '''
-
 
 INSERT_MSG_METADATA_QUERY = '''
     INSERT INTO message_details 
@@ -24,10 +29,21 @@ INSERT_MSG_METADATA_QUERY = '''
             '{sender_email}',
             '{recipient_email}',
             '{msg_snippet}',
-            '{date}',
+            '{msg_receipt_date}',
             '{reply_to}',
             '{list_unsubscribe}',
-            '{return_path}');
+            '{return_path}',
+            STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'),
+            'C');
+'''
+
+# archival_cycle = 'P' for all previous archived messages. Before inserting new records, we update
+#    all existing records to 'P'
+# archival_cycle = 'C' for current batch of messages to be archived
+UPDATE_TABLE_QUERY = '''
+    UPDATE message_details
+    SET archival_cycle = 'P'
+    WHERE archival_cycle = 'C';
 '''
 
 def db_conn(label_name: str) -> object:
@@ -36,10 +52,11 @@ def db_conn(label_name: str) -> object:
     Inputs: label_name -> This attribute is used to create seperate DBs for each label.
             TODO: Explore the possibility of one DB and multiple schemas/tables.
     """
-    db_name = f'gmail_{label_name.lower()}.db' 
+    db_name = f'gmail_{label_name.lower()}.db'
+    db_path = os.path.join(db_folder_path, db_name)
 
     # Connect to DB and create a connection object
-    sqliteConnection = lite.connect(db_name)
+    sqliteConnection = lite.connect(db_path)
     print('Instance of SQLite DB initiated!')
             
     return sqliteConnection
@@ -54,7 +71,7 @@ def db_insert(label_name: str, msg_metadata_list: List[Dict[str, Any]]) -> None:
                                 sender_email = msg_metadata.get('from').replace("'", '"'),
                                 recipient_email = msg_metadata.get('to', '').replace("'", '"'),
                                 msg_snippet = msg_metadata.get('msg_snippet', '').replace("'", '"'),
-                                date = msg_metadata.get('date'),
+                                msg_receipt_date = msg_metadata.get('date'),
                                 reply_to = msg_metadata.get('reply_to', '').replace("'", '"'),
                                 list_unsubscribe = msg_metadata.get('list_unsubscribe', '').replace("'", '"'),
                                 return_path = msg_metadata.get('return_path', '').replace("'", '"')
@@ -68,4 +85,5 @@ def db_insert(label_name: str, msg_metadata_list: List[Dict[str, Any]]) -> None:
     with db_conn(label_name) as sqliteConnObj:
         sqliteConnObj.execute(CREATE_TABLE_QUERY)
         cursor = sqliteConnObj.cursor()
+        cursor.execute(UPDATE_TABLE_QUERY)
         cursor.executescript(str_of_insert_queries)
