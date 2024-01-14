8a674897c9e98e20a7b3979d84bb8f3502705cb2
Kunal Bhattacharya <kunal.jubce@gmail.com>
2024-01-14 12:16:06 +0530

    Added pytest package support and test_auth script

---
+++ tests/test_auth.py
@@ -0,0 +1,10 @@
+import pytest
+from src.main import Clingy
+
+def test_authentication():
+    clingy = Clingy('credentials.json')
+
+    try:
+        clingy.authenticate()
+    except Exception as e:
+        pytest.fail(f"Authentication failed with error: {str(e)}")
